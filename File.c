f7453a495ea8711a403327099e862edfbc87ad4a
raffyManzo <94429207+RaffyManzo@users.noreply.github.com>
2023-03-01 15:40:37 +0000

    added some comments

--- File.c
+++ File.c
@@ -4,42 +4,45 @@
 int minimo(int a[], int n) {
   int i, pos = 0;
 
-  for(i = 1; i < n ; i++) {
-    if(a[i] < a[pos])
+  for (i = 1; i < n; i++) {
+    if (a[i] < a[pos])
       pos = i;
   }
 
   return pos;
 }
 
-void swap(int *a, int* b) {
+void swap(int *a, int *b) {
   int temp = *a;
   *a = *b;
   *b = temp;
 }
 
 void ordina_array(int *a, int n) {
+  // selection sort
   int i, min;
 
-  for(i = 0; i < n - 1; i++) {
+  for (i = 0; i < n - 1; i++) {
+    // a + i verrà ricercato il minimo della parte restante
+    // del vettore ovvero quella non ordinata, partente dall'indirizzo
+    // base + l'indice corrente
     min = minimo(a + i, n - i) + i;
     swap(&a[i], &a[min]);
   }
-
 }
 
-void input_array(int * a, int n) { 
+void input_array(int *a, int n) {
   int i;
 
-  for(i = 0; i < n; i++){
+  for (i = 0; i < n; i++) {
     scanf("%d", &a[i]);
   }
 }
 
-void output_array(int * a, int n) { 
+void output_array(int *a, int n) {
   int i = 0;
 
-  for(; i < n; i++){
+  for (; i < n; i++) {
     printf("%d\t", a[i]);
   }
 }
--- file.h
+++ file.h
@@ -1,5 +1,9 @@
-int minimo(int *, int );
+/*Ritorna il valore della posizione del valore minimo di un vettore di
+ * dimensione n*/
+int minimo(int *, int);
+/*Scambia due variabili intere*/
 void swap(int *, int *);
-void ordina_array(int *, int );
+/*Ordina il vettore mediante l'algoritmo di ordinamento selection sort*/
+void ordina_array(int *, int);
 void input_array(int *, int n);
 void output_array(int *, int n);
